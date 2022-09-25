bb0b689bb6515d0b5b3170c533403fea706244ce
Rodrigom <maceno@gmx.com>
2022-09-24 19:32:52 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 // import Navbar from './components/Navbar';
 // import { BrowserRouter as Router, Routes, Route}
 //     from 'react-router-dom';
-// import Home from './pages';
-// import About from './pages/about';
-// import Blogs from './pages/blogs';
-// import SignUp from './pages/signup';
-// import Contact from './pages/contact';
+
   
 function App() {
 return (
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MyApp } from './MyApp';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Image from 'react-bootstrap/Image'
-import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <App/>
+
     <MyApp />
   
 );
